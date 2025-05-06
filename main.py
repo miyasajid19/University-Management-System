b79b6a46f626e5308f671b31ea5bdcbc222c8e8b
SAJID MIYA <miyasajid19@gmail.com>
2025-05-06 19:24:33 +0530

    project finalized and locked

--- main.py
+++ main.py
@@ -2510,4 +2510,4 @@ def admin_logs():
     display_query = request.args.get('query', querymaker(query, None))
     return render_template('logs.html', logs=logs, query=display_query)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000,debug=True)
+    app.run(host='0.0.0.0', port=8000)
